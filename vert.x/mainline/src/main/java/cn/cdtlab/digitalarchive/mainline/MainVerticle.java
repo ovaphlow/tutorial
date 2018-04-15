33fdf8a8041709f7cddd63ecb040c226b059c778
ovaphlow <jsfreecss@163.com>
2018-04-15 20:47:11 +0800

    .

--- vert.x/mainline/src/main/java/cn/cdtlab/digitalarchive/mainline/MainVerticle.java
+++ vert.x/mainline/src/main/java/cn/cdtlab/digitalarchive/mainline/MainVerticle.java
@@ -1,16 +1,22 @@
 package cn.cdtlab.digitalarchive.mainline;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpServer;
 
 public class MainVerticle extends AbstractVerticle {
 
-    @Override
-    public void start() throws Exception {
-        vertx.createHttpServer().requestHandler(req -> {
-              req.response()
-                .putHeader("content-type", "text/plain")
-                .end("Hello from Vert.x!");
-            }).listen(8080);
-        System.out.println("HTTP server started on port 8080");
-    }
+  @Override
+  public void start() {
+    HttpServer server = vertx.createHttpServer();
+    server.requestHandler(req -> {
+      req.response()
+        .putHeader("content-type", "text/plain")
+        .end("Hello from Vert.x!");
+    }).listen(8080);
+    System.out.println("HTTP server started on port 8080");
+  }
+
+  public static void main(String[] args) {
+    new MainVerticle().start();
+  }
 }

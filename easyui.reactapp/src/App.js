8ab8cf37f446c2295ee61fe22159ddfc06077940
Ovaphlow <ovaphlow@outlook.com>
2019-03-06 22:29:52 +0800

    .

---
+++ easyui.reactapp/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Layout, LayoutPanel } from 'rc-easyui'
+
+import WestTree from './components/WestTree'
+
+class App extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    const titleStyle = {
+      textAlign: 'center',
+      marginTop: '10px'
+    }
+
+    return (
+      <div>
+        <h1>#TITLE#</h1>
+        <Layout style={{ width: '100%', height: '85vh' }}>
+          <LayoutPanel region="north" style={{ height: 50 }}>
+            <div style={titleStyle}>
+              #CAPTION#
+            </div>
+          </LayoutPanel>
+
+          <LayoutPanel region="south" style={{ height: 50 }}>
+            <div style={titleStyle}>
+              #FOOTER#
+            </div>
+          </LayoutPanel>
+
+          <LayoutPanel region="west" style={{ width: 200 }}>
+            {/* <div style={titleStyle}> */}
+              {/* West Region */}
+            {/* </div> */}
+            <WestTree />
+          </LayoutPanel>
+
+          <LayoutPanel region="center" style={{ height: '100%' }}>
+            <div style={titleStyle}>
+            </div>
+          </LayoutPanel>
+        </Layout>
+      </div>
+    )
+  }
+}
+
+export default App

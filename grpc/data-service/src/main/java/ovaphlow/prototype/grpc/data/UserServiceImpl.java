3456b31bdae14f442bd32aadb335da7d1b041f4b
ovaphlow <ovaphlow@live.com>
2020-05-03 20:05:43 +0800

    ...

--- grpc/data-service/src/main/java/ovaphlow/prototype/grpc/data/UserServiceImpl.java
+++ grpc/data-service/src/main/java/ovaphlow/prototype/grpc/data/UserServiceImpl.java
@@ -2,14 +2,14 @@ package ovaphlow.prototype.grpc.data;
 
 import com.google.gson.Gson;
 import io.grpc.stub.StreamObserver;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ArrayListHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -39,28 +39,33 @@ public class UserServiceImpl extends UserGrpc.UserImplBase {
             resp.put("message", "gRPC服务错误");
         }
 
-//        try {
-//            Map<String, Object> body = gson.fromJson(req.getData(), Map.class);
-//            logger.info("", body);
-//            Connection conn = DBUtil.getConn();
-//            String sql = "select * from public.common_user limit 100";
-//            PreparedStatement ps = conn.prepareStatement(sql);
-//            ResultSet rs = ps.executeQuery();
-//            List<Map<String, Object>> result = new ArrayList();
-//            while (rs.next()) {
-//                Map<String, Object> map = new HashMap<>();
-//                ResultSetMetaData rsmd = rs.getMetaData();
-//                int count = rsmd.getColumnCount();
-//                for (int i = 1; i <= count; i++) {
-//                    map.put(rsmd.getColumnLabel(i), rs.getObject(i));
-//                }
-//                result.add(map);
-//            }
-//            resp.put("content", result);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            resp.put("message", "gRPC服务器错误");
-//        }
+        UserReply reply = UserReply.newBuilder().setData(gson.toJson(resp)).build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void signUp(UserRequest req, StreamObserver<UserReply> responseObserver) {
+        Gson gson = new Gson();
+        Map<String, Object> body = gson.fromJson(req.getData(), Map.class);
+        logger.info("", body);
+        Map<String, Object> resp = new HashMap<>();
+        resp.put("message", "");
+        resp.put("content", "");
+
+        try (Connection conn = DBUtil.getConnection()) {
+            String sql = "insert into public.common_user (uuid, username, password" +
+                    "values (?, ?, ?) " +
+                    "returning id";
+            new QueryRunner()
+                    .query(conn, sql, new ArrayListHandler())
+                    .stream();
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.put("message", "gRPC服务错误");
+        }
 
         UserReply reply = UserReply.newBuilder().setData(gson.toJson(resp)).build();
         responseObserver.onNext(reply);

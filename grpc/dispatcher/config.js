2ee78e79a5b95d499bef33063d2e36360af31975
ovaphlow <ovaphlow@live.com>
2020-05-03 19:30:25 +0800

    ...

--- grpc/dispatcher/config.js
+++ grpc/dispatcher/config.js
@@ -4,7 +4,14 @@ const config = {
   env: 'development',  
   app: {
     port: 5000,
-    numChildProcesses: os.cpus().length
+    numChildProcesses: os.cpus().length,
+    namespace: 'ovaphlow@CDT1123',
+    // config.app.namespace.split('').map(it => it.charCodeAt())
+    namespaceByteArray: [
+      111, 118, 97, 112, 104, 108,
+      111, 119, 64,  67,  68,  84,
+      49,  49, 50,  51
+    ]
   },
   grpcServer: {
     host: '127.0.0.1',
@@ -13,18 +20,6 @@ const config = {
       'grpc.max_send_message_length': 1024 * 1024 * 256,
       'grpc.max_receive_message_length': 1024 * 1024 * 256,
     }
-  }
-}
-
-const prodConfig = {
-  env: 'production',
-  app: {
-    port: 5000,
-    numChildProcesses: os.cpus().length
-  },
-  grpcServer: {
-    host: '127.0.0.1',
-    port: 5001
   }
 }
 
--- grpc/dispatcher/routes/user.js
+++ grpc/dispatcher/routes/user.js
@@ -1,6 +1,7 @@
 const grpc = require('grpc')
 const protoLoader = require('@grpc/proto-loader')
 const Router = require('@koa/router')
+const uuid = require('uuid')
 
 const config = require('../config')
 
@@ -23,27 +24,50 @@ const router = new Router({
 
 module.exports = router
 
-router
-  .get('/sign-in', async ctx => {
-    const fetch = body => {
-      return new Promise((resolve, reject) => {
-        grpcClient.signIn({data: JSON.stringify(body)}, (err, response) => {
-          if (err) {
-            console.error(err)
-            reject(err)
-            return
-          }
-          resolve(JSON.parse(response.data))
-        })
+router.get('/sign-in', async ctx => {
+  const fetch = body => {
+    return new Promise((resolve, reject) => {
+      grpcClient.signIn({data: JSON.stringify(body)}, (err, response) => {
+        if (err) {
+          console.error(err)
+          reject(err)
+          return
+        }
+        resolve(JSON.parse(response.data))
       })
-    }
-    try {
-      ctx.response.body = await fetch({
-        username: 'test',
-        password: '1123'
+    })
+  }
+  try {
+    ctx.response.body = await fetch(ctx.request.query)
+  } catch (err) {
+    console.error(err)
+    ctx.response.body = {message: '服务器错误'}
+  }
+})
+
+router.get('/sign-up', async ctx => {
+  const fetch = body => {
+    return new Promise((resolve, reject) => {
+      grpcClient.signIn({data: JSON.stringify(body)}, (err, response) => {
+        if (err) {
+          console.error(err)
+          reject(err)
+          return
+        }
+        resolve(JSON.parse(response.data))
       })
-    } catch (err) {
-      console.error(err)
-      ctx.response.body = {message: '服务器错误'}
+    })
+  }
+  try {
+    if (!!!ctx.request.query.username || !!!ctx.request.query.password) {
+      ctx.response.body = { message: '请完整填写所需信息', content: '' }
+      return
     }
-  })
+    ctx.response.body = await fetch(Object.assign{
+      uuid: uuid.v5(ctx.request.query.username, config.app.namespaceByteArray)
+    }, ctx.request.search)
+  } catch (err) {
+    console.error(err)
+    ctx.response.body = {message: '服务器错误'}
+  }
+})
